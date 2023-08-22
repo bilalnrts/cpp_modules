c892926c1a3ca1fb7958f6818db954e3e61aadf7
Bilal Salih Nurtas <52494435+bilalnrts@users.noreply.github.com>
2023-08-22 16:44:25 +0300

    cpp00_ex00 done

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			std::cout << (char)toupper(av[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
