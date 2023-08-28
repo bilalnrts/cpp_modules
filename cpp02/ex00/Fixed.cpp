f227ea1139da3a23337c530f8da2aa3c665903a3
Bilal Salih Nurtas <bsalihnrts@gmail.com>
2023-08-28 17:42:00 +0300

    cpp02_ex00 copy constructor and destructor done

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -3,3 +3,13 @@
 Fixed::Fixed() : _value(0)
 {
 }
+
+Fixed::Fixed(Fixed &another)
+{
+	*this = another;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -1,6 +1,8 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed
 {
 	private :
@@ -8,6 +10,7 @@ class Fixed
 		static const int	_fixedvale = 8;
 	public :
 		Fixed(void);
+		Fixed(Fixed &another);
 		~Fixed();
 		int	getRawBits(void) const;
 		void	setRawBits(int const raw);
